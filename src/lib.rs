9861359c6f7138c57a95bfdbb78d93584303cdc5
khai96_ <hvksmr1996@gmail.com>
2023-03-07 12:02:56 +0700

    Create

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+use derive_more::{Display, Error};
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "{name}: {source}")]
+pub struct MyError<Name: Display, Source: Error> {
+    pub name: Name,
+    #[error(source)]
+    pub source: Source,
+}
